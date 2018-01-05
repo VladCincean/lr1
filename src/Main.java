08922db8536bfc104fd3e6123025fc4a663d6ad4
Vlad CINCEAN <cvie1883@scs.ubbcluj.ro>
2018-01-05 11:47:14 +0200

    Refactored FIRST1 and FOLLOW1 code and implemented canonical collection algorithms; some things need to be tested

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import grammar.ContextFreeGrammar;
+import grammar.FirstAndFollow;
 import grammar.GrammarBuilder;
 import model.symbol.Symbol;
 import model.symbol.Terminal;
@@ -9,9 +10,10 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         ContextFreeGrammar cfg = GrammarBuilder.buildFromFile("res/grammar4.txt");
+        FirstAndFollow ff = new FirstAndFollow(cfg);
 
-        Map<Symbol, Set<Terminal>> first = cfg.getFirst1();
-        Map<Symbol, Set<Terminal>> follow = cfg.getFollow1();
+        Map<Symbol, Set<Terminal>> first = ff.getFirst1();
+        Map<Symbol, Set<Terminal>> follow = ff.getFollow1();
 
         System.out.println(first.toString());
         System.out.println(follow.toString());
--- src/parser/lr1/CanonicalCollection.java
+++ src/parser/lr1/CanonicalCollection.java
@@ -1,7 +1,11 @@
 package parser.lr1;
 
 import grammar.ContextFreeGrammar;
+import grammar.FirstAndFollow;
+import model.ProductionRuleCFG;
+import model.symbol.Nonterminal;
 import model.symbol.Symbol;
+import model.symbol.Terminal;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +13,7 @@ import java.util.List;
 public class CanonicalCollection {
     private List<State> states;
     private ContextFreeGrammar G;
+    private FirstAndFollow ff;
 
     /**
      * CanonicalCollection constructor
@@ -16,9 +21,13 @@ public class CanonicalCollection {
      */
     public CanonicalCollection(ContextFreeGrammar G) {
         this.G = G;
-        this.states = new ArrayList<>();
+        this.ff = new FirstAndFollow(this.G);
 
         buildCanonicalCollection();
+    }
+
+    public List<State> getTheCanonicalCollection() {
+        return states;
     }
 
     /**
@@ -28,18 +37,78 @@ public class CanonicalCollection {
      */
     private void buildCanonicalCollection() {
         // TODO: this
+
+        List<State> C1 = new ArrayList<>();
+
+        ProductionRuleCFG p0 = null;
+        for (ProductionRuleCFG p : G.getProductionRules()) {
+            if (p.getId() == 0) {
+                p0 = p;
+                break;
+            }
+        }
+
+        State S0 = closure(new Element(p0, 0, Terminal.getDollarSymbol()));
+        C1.add(S0);
+
+        boolean changes = true;
+        do {
+            changes = false;
+
+            // for all s in C1 do
+            for (State s : C1) {
+                // for all X nonterminal or terminal do
+                for (Nonterminal X : G.getNonterminals()) {
+                    State T = goto1(s, X);
+
+                    // if T not empty and T not in C1 then add it to C1
+                    if ((T.getElements().size() > 0) && (!C1.contains(T))) {
+                        changes = true;
+                        C1.add(T);
+                    }
+                }
+                for (Terminal X : G.getTerminals()) {
+                    State T = goto1(s, X);
+
+                    // if T not empty and T not in C1 then add it to C1
+                    if ((T.getElements().size() > 0) && (!C1.contains(T))) {
+                        changes = true;
+                        C1.add(T);
+                    }
+                }
+            }
+        } while (changes);
+
+        this.states = C1;
     }
 
-    private State goto1(State oldState, Symbol s) {
-        State newState = null;
+    /**
+     * Goto function
+     * goto1(s, X) = closure({[A -> alpha X . beta] | [A -> alpha . X beta] in s})
+     * @param s - old state
+     * @param X - symbol
+     * @return  new state
+     */
+    private State goto1(State s, Symbol X) {
+        //  TODO: !!!!
+        //  TODO: something is wrong here, figure out what
+        //  TODO: !!!!
+        State s1 = new State();
 
-        // TODO: this
+        for (Element e : s.getElements()) {
+            //  if [A -> alpha . X beta, u] in s
+            //      then add [A -> alpha X . beta, u] in s1
+            if (e.getCore().getRight().get(e.getDot()).equals(X)) {
+                s1.addElement(new Element(e.getCore(), e.getDot() + 1, e.getPrediction()));
+            }
+        }
 
-        return newState;
+        return s1;
     }
 
     /**
      * Closure algorithm.
+     * TODO: test this
      * @param I - analysis element
      * @return closure(I)
      * Input:   I   - analysis element
@@ -57,7 +126,34 @@ public class CanonicalCollection {
             changes = false;
 
             // for all [A -> alpha . B beta, a] in C1 do
-            // TODO: continue to implement from here
+            for (Element e : C1.getElements()) {
+                if (e.getCore().getRight().get(e.getDot()) instanceof Nonterminal) {
+                    Nonterminal A = e.getCore().getLeft();
+                    List<Symbol> alpha = e.getCore().getRight().subList(0, e.getDot());
+                    Nonterminal B = (Nonterminal) e.getCore().getRight().get(e.getDot());
+                    List<Symbol> beta = e.getCore().getRight().subList(e.getDot() + 2, e.getCore().getRight().size());
+                    Terminal a = e.getPrediction();
+
+                    // for all "B -> gamma" production rule do
+                    for (ProductionRuleCFG p : G.getProductionRules()) {
+                        if (p.getLeft().equals(B)) {
+                            List<Symbol> gamma = p.getRight();
+
+                            // for all b in FIRST(beta alpha) do
+                            List<Symbol> betaAlpha = new ArrayList<>();
+                            betaAlpha.addAll(beta);
+                            betaAlpha.addAll(alpha);
+                            for (Terminal b : ff.getFirstOfSequence(betaAlpha)) {
+                                // if [B -> . gamma, b] not in C1 then add it
+                                Element potentialNewElement = new Element(p, 0, b);
+                                if (!C1.getElements().contains(potentialNewElement)) {
+                                    C1.addElement(potentialNewElement);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         } while (changes);
 
         return C1;
